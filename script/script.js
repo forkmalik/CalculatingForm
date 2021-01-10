c3e1d516c9f4c84b6d46371bae429976b7bf6235
Vlad Savchenko <polkamaraka@gmail.com>
2021-01-10 21:28:24 +0200

    rewrite validation

--- script/script.js
+++ script/script.js
@@ -1,142 +1,169 @@
-var valid = true;
+document.addEventListener("DOMContentLoaded", () => {
+  "use strict";
 
-let packTick = document.getElementById("packaging-box");
-packTick.addEventListener("change", (event) => {
-  let packBlock = document.getElementById("package-block");
-  if (packTick.checked) {
-    packBlock.style.display = "Inline";
-  } else {
-    packBlock.style.display = "None";
+  const form = document.getElementById("calc");
+  const packTick = document.getElementById("packaging-box");
+  const returnTick = document.getElementById("return-box");
+  const parent = document.getElementById("place");
+  const placeInputs = parent.getElementsByTagName("input");
+  let isValid = false;
+
+  const regExpString = /^[A-Z]{1}[a-z]{1,}$|^[A-Z]{1}[a-z]{1,}\-[A-Z]{1}[a-z]{1,}$/;
+  const regExpDigits = /^[1-9]{1,}$|^[1-9]{1,}[0-9]{1,}$/;
+
+  const validate = (elem) => {
+    switch (elem.name) {
+      case "from":
+        matchText(elem);
+        break;
+      case "to":
+        matchText(elem);
+        break;
+      case "count":
+        matchDigits(elem);
+        break;
+      case "price":
+        matchDigits(elem);
+        break;
+      case "weight":
+        matchDigits(elem);
+        break;
+      case "height":
+        matchDigits(elem);
+        break;
+      case "width":
+        matchDigits(elem);
+        break;
+      case "length":
+        matchDigits(elem);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const matchText = (elem) => {
+    if (!regExpString.test(elem.value) && elem.value !== "") {
+      elem.style.borderColor = "Red";
+    } else {
+      elem.style.borderColor = "Initial";
+    }
+  };
+
+  const matchDigits = (elem) => {
+    if (!regExpDigits.test(elem.value) && elem.value !== "") {
+      elem.style.borderColor = "Red";
+    } else {
+      elem.style.borderColor = "Initial";
+    }
+  };
+
+  const calcDeliver = (fields) => {
+    for (i = 0; i < fields.length; i++) {
+      fields[i] = parseInt(fields[i].value, 10);
+    }
+
+    let result =
+      (0.05 * parseInt(fields[1].value, 10) +
+        (parseInt(fields[2].value, 10) + 0.1) +
+        (parseInt(fields[3].value, 10) *
+          parseInt(fields[4].value, 10) *
+          parseInt(fields[5].value, 10)) /
+          4000) *
+      parseInt(fields[0].value, 10);
+    console.log(result);
+    let packCheck = document.getElementById("packaging-box");
+    if (packCheck.checked) {
+      let boxSize = document.getElementById("package-type").value;
+      let boxCount = document.getElementById("package-amount").value;
+      switch (boxSize) {
+        case "1":
+          result += boxCount * 8;
+          break;
+        case "5":
+          result += boxCount * 14;
+          break;
+        case "10":
+          result += boxCount * 19;
+          break;
+        case "15":
+          result += boxCount * 22;
+          break;
+        default:
+          result = result;
+      }
+    }
+    if (floorInpt.value) {
+      let elevator = document.getElementById("elevator-box");
+      if (elevator.checked) {
+        result += 20;
+      } else {
+        result += floorInpt.value * 5;
+      }
+    }
+
+    let returnShip = document.getElementById("return-box");
+    if (returnShip.checked) {
+      let returnType = document.getElementById("return-type");
+      switch (returnType.value) {
+        case "money":
+          result += 3;
+          break;
+        case "documents":
+          result += 5;
+        default:
+          result = result;
+      }
+    }
+    console.log(result);
+    return result;
+  };
+
+  for (let elem of form.elements) {
+    if (elem.classList.contains("validate")) {
+      elem.addEventListener("blur", () => {
+        validate(elem);
+      });
+    }
   }
-});
-let returnTick = document.getElementById("return-box");
-returnTick.addEventListener("change", (event) => {
-  let returnBlock = document.getElementById("return-block");
-  if (returnTick.checked) {
-    returnBlock.style.display = "Block";
-  } else {
-    returnBlock.style.display = "None";
-  }
-});
 
-let calcBtn = document.getElementById("calc-btn");
-calcBtn.addEventListener("click", (event) => {
-  event.preventDefault();
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    for (let elem of form.elements) {
+      if (elem.classList.contains("validate")) {
+        if (elem.value === "") {
+          elem.style.borderColor = "Red";
+          isValid = false;
+        } else {
+          isValid = true;
+        }
+      }
+    }
+  });
 
-  let fromRoute = document.getElementById("from-route");
-  let toRoute = document.getElementById("to-route");
-  let fromRight = matchCity(fromRoute);
-  let toRight = matchCity(toRoute);
+  packTick.addEventListener("change", (event) => {
+    let packBlock = document.getElementById("package-block");
+    if (packTick.checked) {
+      packBlock.style.display = "Inline";
+    } else {
+      packBlock.style.display = "None";
+    }
+  });
 
-  validateInput(fromRoute, fromRight);
-  validateInput(toRoute, toRight);
-
-  let parent = document.getElementById("place");
-  var placeInputs = parent.getElementsByTagName("input");
-
-  for (i = 0; i < placeInputs.length; i++) {
-    let rightValue = matchDigits(placeInputs[i]);
-    validateInput(placeInputs[i], rightValue);
-  }
-
+  returnTick.addEventListener("change", (event) => {
+    let returnBlock = document.getElementById("return-block");
+    if (returnTick.checked) {
+      returnBlock.style.display = "Block";
+    } else {
+      returnBlock.style.display = "None";
+    }
+  });
   
-
-  let result = calcDeliver(placeInputs);
-  if (valid) {
+  if (isValid) {
+    let result = calcDeliver(placeInputs);
     let resultBlock = document.getElementById("result-block");
     resultBlock.style.display = "Block";
-    let resultText = document.getElementById('result__price');
+    let resultText = document.getElementById("result__price");
     resultText.innerText = result + "UAH";
   }
-  
-  
 });
-
-var calcDeliver = (fields) => {
-  for (i = 0; i < fields.length; i++) {
-    fields[i] = parseInt(fields[i].value, 10);
-    console.log(parseInt(fields[i].value, 10)); 
-  }
-
-  let result = ((0.05 * parseInt(fields[1].value, 10) + (parseInt(fields[2].value, 10) + 0.1) + (parseInt(fields[3].value, 10) * parseInt(fields[4].value, 10) * parseInt(fields[5].value, 10)) / 4000)) * parseInt(fields[0].value, 10);
-  console.log(result);
-  let packCheck = document.getElementById("packaging-box");
-  if (packCheck.checked) {
-    let boxSize = document.getElementById("package-type").value;
-    let boxCount = document.getElementById("package-amount").value;
-    switch (boxSize) {
-      case '1':
-        result += boxCount * 8;
-        break;
-      case '5':
-        result += boxCount * 14;
-        break;
-      case '10':
-        result += boxCount * 19;
-        break;
-      case '15':
-        result += boxCount * 22;
-        break;
-      default:
-        result = result;
-    }
-  }
-  if (floorInpt.value) {
-    let elevator = document.getElementById("elevator-box");
-    if (elevator.checked) {
-      result += 20;
-    } else {
-      result += floorInpt.value * 5;
-    }
-  }
-
-  let returnShip = document.getElementById("return-box");
-  if (returnShip.checked) {
-    let returnType = document.getElementById('return-type');
-    switch (returnType.value) {
-      case "money":
-        result += 3;
-        break;
-      case "documents":
-        result += 5;
-      default:
-        result = result;
-    }
-  }
-  console.log(result);
-  return result;
-};
-
-var floorInpt = document.getElementById("floor-field");
-floorInpt.addEventListener("input", () => {
-  let correct = matchDigits(floorInpt);
-  validateInput(floorInpt, correct);
-});
-
-var validateInput = (field, value) => {
-  if (!value) {
-    valid = false;
-    changeColor(field, "Red");
-  }
-  if (value) {
-    valid = true;
-    changeColor(field, "Initial");
-  }
-};
-
-var matchDigits = (elem) => {
-  let rightForm = elem.value.match(/^[1-9]{1,}$|^[1-9]{1,}[0-9]{1,}$/);
-  return rightForm;
-};
-
-var matchCity = (elem) => {
-  let rightForm = elem.value.match(/^[A-Z]{1}[a-z]{1,}$/);
-  if (!rightForm) {
-    rightForm = elem.value.match(/^[A-Z]{1}[a-z]{1,}\-[A-Z]{1}[a-z]{1,}$/);
-  }
-  return rightForm;
-};
-
-var changeColor = (element, color) => {
-  element.style.borderColor = color;
-};
